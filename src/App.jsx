ff468f6ff59f557b03c2ace0886fa78345106b80
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2022-01-14 16:21:09 +0200

    deploy gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,48 @@
 import './App.scss';
-import { Navbar, Home, News, Cryptocurrencies, Exchanges, CryptoDetails, Footer  } from './components';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  Navbar,
+  Home,
+  News,
+  Cryptocurrencies,
+  Exchanges,
+  CryptoDetails,
+  Footer,
+} from './components';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
-
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-    <div className="app">
-        <Navbar />
-        <div className="routes">
-          <div className='main_content'>
-          <Switch>
-            <Route exact path={'/'}>
-              <Home />
-            </Route>
-            <Route  path={'/cryptocurrencies'}>
-              <Cryptocurrencies />
-            </Route>
-            <Route  path={'/crypto/:coinId'}>
-              <CryptoDetails />
-            </Route>
-            <Route path={'/exchanges'}>
-              <Exchanges />
-            </Route>
-            <Route path={'/news'}>
-              <News />
-            </Route>
-          </Switch>
+      <HashRouter>
+        <div className="app">
+          <Navbar />
+          <div className="routes">
+            <div className="main_content">
+              <Switch>
+                <Route exact path={'/'}>
+                  <Home />
+                </Route>
+                <Route path={'/cryptocurrencies'}>
+                  <Cryptocurrencies />
+                </Route>
+                <Route path={'/crypto/:coinId'}>
+                  <CryptoDetails />
+                </Route>
+                <Route path={'/exchanges'}>
+                  <Exchanges />
+                </Route>
+                <Route path={'/news'}>
+                  <News />
+                </Route>
+              </Switch>
+            </div>
+            <footer className="footer_content">
+              <Footer />
+            </footer>
           </div>
-          <footer className='footer_content'>
-
-          <Footer/>
-          </footer>
         </div>
-    </div>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 };
